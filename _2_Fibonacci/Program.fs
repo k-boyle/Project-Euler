05adc5e5653b1e1b207c507ff2d08de6fd3c21cb
Casino Boyale <kayrenhi@gmail.com>
2020-05-29 21:57:50 +0100

    Problem 2 solution

---
+++ _2_Fibonacci/Program.fs
@@ -0,0 +1,11 @@
+﻿[<EntryPoint>]
+let main argv =
+    let fib = Seq.unfold (fun (x, y) -> Some(x + y, (y, x + y))) (1, 1)
+    let sum =
+        fib
+        |> Seq.takeWhile (fun x -> x <= 4000000)
+        |> Seq.filter (fun x -> x % 2 = 0)
+        |> Seq.sum
+        
+    printfn "%d" sum
+    0 // return an integer exit code
