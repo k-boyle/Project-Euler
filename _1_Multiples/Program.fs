b77320d954d41ec7866d4fa6f2ee1e309a6094fd
Casino Boyale <kayrenhi@gmail.com>
2020-05-29 21:01:53 +0100

    Problem 1 solution

---
+++ _1_Multiples/Program.fs
@@ -0,0 +1,13 @@
+﻿let multipleof x y = x % y = 0
+
+[<EntryPoint>]
+let main argv =
+    let multiples = seq {
+        for i in 1 .. 999 do
+            let curr = multipleof i
+            if curr 3 || curr 5 then
+                i
+    }
+    
+    printfn "%d" (multiples |> Seq.sum)
+    0 // return an integer exit code
