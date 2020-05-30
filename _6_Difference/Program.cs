40fb7a17a2b261d7983a80baa028bcc010bc38e2
Casino Boyale <kayrenhi@gmail.com>
2020-05-30 20:55:27 +0100

    Problem 6 solution

---
+++ _6_Difference/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _6_Difference {
+    class Program {
+        static void Main(string[] args) {
+            //difference is quartic sequence https://oeis.org/A052149
+            //a(n) = n*(n-1)*(n+1)*(3*n+2)/12
+            Func<int, int> a = i => i * (i - 1) * (i + 1) * (3 * i + 2) / 12;
+            Console.WriteLine(a(100));
+        }
+    }
+}
