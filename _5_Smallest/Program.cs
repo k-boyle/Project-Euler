9c26142d3d8cc27466af0fc7f7469dd77a38b0e6
Casino Boyale <kayrenhi@gmail.com>
2020-05-30 18:02:36 +0100

    Got rid of meme

--- _5_Smallest/Program.cs
+++ _5_Smallest/Program.cs
@@ -4,21 +4,13 @@ using System.Linq;
 
 namespace _5_Smallest {
     class Program {
-        private static int counter = 20;
-        
         static void Main(string[] args) {
             var divisors = Enumerable.Range(2, 19);
-            Console.WriteLine(InfiniteEnumerable().First(i => IsDivisable(i, divisors)));
+            Console.WriteLine(Enumerable.Range(20, int.MaxValue - 20).First(i => IsDivisable(i, divisors)));
         }
         
         private static bool IsDivisable(int numerator, IEnumerable<int> divisors) {
             return divisors.All(x => numerator % x == 0);
-        }
-        
-        private static IEnumerable<int> InfiniteEnumerable() {
-            while (true) {
-                yield return counter++;   
-            }
         }
     }
 }
