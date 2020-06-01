5809e2dbf7b38632cef8f468e093d344b8bf08ec
Kieran Boyle <kayrenhi@gmail.com>
2020-06-01 23:01:41 +0100

    Fixed double space

--- _3_Primes/Main.java
+++ _3_Primes/Main.java
@@ -3,7 +3,7 @@ package com.casino;
 public class Main {
     public static void main(String[] args) {
         long numerator = 600851475143L;
-        long denominator  = 2;
+        long denominator = 2;
         do {
             if (numerator % denominator  == 0) {
                 numerator = numerator / denominator ;
