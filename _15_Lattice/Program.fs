60d32123d5e4638eef35f327e58e9abb4b3de0db
Casino Boyale <kayrenhi@gmail.com>
2020-06-14 00:09:29 +0100

    Problem 15 solution

---
+++ _15_Lattice/Program.fs
@@ -0,0 +1,15 @@
+﻿
+let rec factorial n =
+    if n = 1I then
+        n
+    else
+       n * (factorial <| n - 1I)
+
+let ncr n r = factorial n / (factorial r * factorial (n - r))
+
+[<EntryPoint>]
+let main argv =
+    let width = 20I
+    let height = 20I
+    printfn "%A" <| ncr (width + height) width
+    0
