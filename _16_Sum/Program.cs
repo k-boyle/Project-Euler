84894396e90ccefe06139c34a44a1f8bea0938d0
Casino Boyale <kayrenhi@gmail.com>
2020-06-14 17:09:53 +0100

    Problem 16 solution

---
+++ _16_Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+using System;
+
+namespace _16_Sum {
+    class Program {
+        static void Main(string[] args) {
+            BigInteger number = BigInteger.Pow(2, 1000);
+            Console.WriteLine(SumDigits(number));
+        }
+
+        static BigInteger SumDigits(BigInteger number) {
+            if (number == 0) {
+                return 0;
+            }
+
+            return number % 10 + SumDigits(number / 10);
+        }
+    }
+}
