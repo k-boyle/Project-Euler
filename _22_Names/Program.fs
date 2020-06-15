eb92f820fb62f4d0c5c6c9934a6340c876a05540
Kieran <kayrenhi@gmail.com>
2020-06-15 18:19:51 +0100

    Problem 22 solution

---
+++ _22_Names/Program.fs
@@ -0,0 +1,30 @@
+﻿open System
+open System.IO
+
+let getNames path = 
+    async {
+        let! rawText = File.ReadAllTextAsync path |> Async.AwaitTask
+        return rawText.Split (',', StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map (fun str -> str.Replace ("\"", ""))
+        |> Array.sort
+    }
+
+let getNameSum (name: string) =
+    name.ToCharArray()
+    |> Array.map (fun c -> (bigint (int c)) - 64I) |> Array.reduce (+)
+
+let getNameValue index value =
+    bigint (index + 1) * value
+
+[<EntryPoint>]
+let main argv =
+    let namesLoc = "./names.txt"
+    let names = getNames namesLoc |> Async.RunSynchronously
+    let total = 
+        names
+        |> Array.map getNameSum
+        |> Array.mapi getNameValue
+        |> Array.reduce (+)
+        
+    printfn "%A" total
+    0
