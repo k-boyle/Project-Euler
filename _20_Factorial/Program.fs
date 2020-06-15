385101780ab54ed2d978696390f7d9bf0055d392
Kieran <kayrenhi@gmail.com>
2020-06-15 10:59:58 +0100

    Problem 20 solution

---
+++ _20_Factorial/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+let rec factorial n =
+    match n with
+    | _ when n > 1I -> n * factorial (n - 1I)
+    | _ -> 1I
+
+let rec sumDigits n =
+    match n with
+    | _ when n > 0I -> n % 10I + sumDigits (n / 10I)
+    | _ -> 0I
+
+[<EntryPoint>]
+let main argv =
+    let sum = 100I |> factorial |> sumDigits
+    printfn "%A" sum
+    0
