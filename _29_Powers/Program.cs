9aaeb40d1e94624a521c05ce8fe71245d1dc98a8
Kieran <kayrenhi@gmail.com>
2020-06-18 22:03:38 +0100

    Problem 29 solution

---
+++ _29_Powers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+using System;
+using System.Collections.Generic;
+
+namespace _29_Powers {
+    class Program     {
+        static void Main(string[] args) {
+            HashSet<BigInteger> results = new HashSet<BigInteger>();
+            for (int a = 2; a <= 100; a++) {
+                for (int b = 2; b <= 100; b++) {
+                    results.Add(BigInteger.Pow(a, b));
+                }
+            }
+
+            Console.WriteLine(results.Count);
+        }
+    }
+}
