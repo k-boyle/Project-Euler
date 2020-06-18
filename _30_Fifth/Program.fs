dd25e7e9c9cb7eaaaa0da0ac940556ec8bffd4dc
Kieran <kayrenhi@gmail.com>
2020-06-18 23:10:33 +0100

    Problem 30 solution

---
+++ _30_Fifth/Program.fs
@@ -0,0 +1,24 @@
+﻿let getDigits n =
+    seq {
+        let mutable i = n
+        while (i > 0) do
+            yield float <| i % 10
+            i <- i / 10
+    }
+
+let isPartialNarcissistic n =
+    n
+    |> getDigits
+    |> Seq.map (fun i -> int (i ** 5.0))
+    |> Seq.reduce (+)
+    |> n.Equals
+
+[<EntryPoint>]
+let main argv =
+    let numbers = [ 2..999999 ]
+    let narcissistics =
+        numbers
+        |> List.filter isPartialNarcissistic
+        |> List.reduce (+)
+    printf "%A" narcissistics
+    0
