28e3320107adcbeb70b8dd8cad92c7ec15495878
Kieran <kayrenhi@gmail.com>
2020-06-19 21:25:10 +0100

    Problem 31 solution

---
+++ _31_Change/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _31_Change {
+    class Program {
+        static void Main(string[] args) {
+            var coins = new int[] { 1, 2, 5, 10, 20, 50, 100, 200 };
+            Console.WriteLine(Possibilities(200, coins));
+        }
+
+        static int Possibilities(int target, int[] coins) {
+            var permutations = new int[target + 1];
+            permutations[0] = 1;
+
+            foreach (int coin in coins) {
+                for (int i = coin; i <= target; i++) {
+                    permutations[i] += permutations[i - coin];
+                }
+            }
+
+            return permutations[target];
+        }
+    }
+}
